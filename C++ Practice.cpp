c1aa0e32cd5146c1053f53cb4a064238a3a7b809
Ewan <StrikerKagami@Gmail.com>
2021-04-08 07:51:34 +0100

    Classes overview

--- C++ Practice.cpp
+++ C++ Practice.cpp
@@ -2,6 +2,7 @@
 //
 #include "Basics/Functions.h"
 #include <iostream>
+#include "Basics/Classes.h"
 
 int main()
 {
@@ -23,15 +24,8 @@ int main()
     int PassByReference = -8;
     OutputByReference(PassByReference);
     std::cout << "New Base Value : " << PassByReference << std::endl;
+
+    Animal cow("Bessie", 14, false);
+    cow.Feed();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
